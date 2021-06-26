e4e3ea91d9ff3b583e011f5bd5ace812c17a5e15
Luis Milberg <luismilberg@gmail.com>
2021-06-25 23:34:28 -0300

    Filtrado de meetis por fecha actual

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,14 +1,21 @@
  const Grupos = require('../models/Grupos');
  const Meeti = require('../models/Meeti');
  const moment = require('moment');
+ const Sequelize = require('sequelize');
+ const Op = Sequelize.Op;
 
 exports.panelAdministracion = async (req, res) => {
+
 
     // Consultas
     const consultas = [];
     
-    consultas.push(Grupos.findAll({wehre: {usuarioId : req.user.id}}));
-    consultas.push(Meeti.findAll({where: {usuarioId : req.user.id}}));
+    consultas.push(Grupos.findAll({where: {usuarioId : req.user.id}}));
+    consultas.push(Meeti.findAll({
+        where: {
+            usuarioId : req.user.id,
+            fecha: {[Op.gte] : moment(new Date()).format("YYYY-MM-DD")}
+        }}));
 
     const [grupos, meetis] = await Promise.all(consultas);
 
